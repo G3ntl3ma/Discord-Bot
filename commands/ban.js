becf7ad4b7b6203a348f1fb67c3bff2105f4ccbd
G3ntl3ma <120450774+G3ntl3ma@users.noreply.github.com>
2023-03-04 17:09:17 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,11 +2,12 @@ module.exports = {
     name : 'ban',
     description : 'Ban a person',
     execute(message, args){
-        const member = message.mentions.users.first();
-        if(member){
-            const memberTarget = message.guild.members.cache.get(member.id)
-            memberTarget.ban();
-            message.channel.send('The user ' + member + ' has been banned')
+        const members = message.mentions.users;
+        if(members){
+            members.forEach(member => {
+                const m = message.guild.members.cache.get(member.id)
+                m.ban()
+                message.channel.send('The user ' + member.toString() + ' has been banned')})
         } else {
             message.channel.send('You cannot ban this member');
         }
