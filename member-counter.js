e29ef5ed8eed6218e81f1fcfd2807e7de5559ab5
G3ntl3ma <120450774+G3ntl3ma@users.noreply.github.com>
2023-03-05 23:19:35 +0100

    Add files via upload

---
+++ member-counter.js
@@ -0,0 +1,9 @@
+module.exports = async (client) => {
+        const guild = client.guilds.cache.get('579292393547759643');
+        setInterval(() =>{
+            const memberCount = guild.memberCount;
+            const channel = guild.channels.cache.get('1082057329345822741');
+            channel.setName(`Total Members: ${memberCount.toLocaleString()}`)
+            console.log('Updating Membercount')
+        }, 5000);
+}
